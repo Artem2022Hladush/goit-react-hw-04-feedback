a9eca1747166d7cc38756351e7e206d8cf0f9c56
Артем Гладуш <m1hommage5008@gmail.com>
2022-12-25 12:45:24 +0200

    refact button

--- src/components/FeedbackButton/FeedbackButton.jsx
+++ src/components/FeedbackButton/FeedbackButton.jsx
@@ -7,9 +7,21 @@ import css from "../FeedbackButton/FeedbackButton.module.css"
 const FeedbackButton = ({options, onAddFeedback}) => {
 return (
 	<div className={css.buttons__list}>
-	<button type="button" className={css.button} onClick={() => onAddFeedback(options[0])}><BsFillHandThumbsUpFill/>Good</button>
+		{options.map(option => (
+			<button 
+			key={option}
+			onClick={()=> onAddFeedback(option)}
+			className={css.button} 
+			>
+				{option === 'good' && <BsFillHandThumbsUpFill size={30} />}
+              {option === 'neutral' && <BsFillHexagonFill size={26} />}
+              {option === 'bad' && <BsFillHandThumbsDownFill size={30} />}
+				  {option}
+			</button>
+		))}
+	{/* <button type="button" className={css.button} onClick={() => onAddFeedback(options[0])}><BsFillHandThumbsUpFill/>Good</button>
 	<button type="button" className={css.button}  onClick={() => onAddFeedback(options[1])}><BsFillHexagonFill/>Neutral</button>
-	<button type="button" className={css.button}  onClick={() => onAddFeedback(options[2])}><BsFillHandThumbsDownFill/>Bad</button>
+	<button type="button" className={css.button}  onClick={() => onAddFeedback(options[2])}><BsFillHandThumbsDownFill/>Bad</button> */}
 </div>
 )
 };
